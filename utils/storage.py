a98e7e5a0de5b1812139ee98a1208b1ee6816e16
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-06-12 11:08:43 +0300

    feat: add storage

--- utils/storage.py
+++ utils/storage.py
@@ -6,3 +6,13 @@ data_file = "data/data.json"
 def save_data(data):
     with open(data_file, "w") as file:
         json.dump(data, file, indent=4)
+
+def load_data():
+    if not os.path.exists(data_file):
+        return {}
+    
+    try:
+        with open(data_file, "r") as file:
+            return json.load(file)
+    except json.JSONDecodeError:
+        return {}
