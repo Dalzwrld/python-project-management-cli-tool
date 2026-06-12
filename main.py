1bf8aa9031ba96b4779e081066f447cdc02b0b9e
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-06-12 11:17:36 +0300

    feat: add main

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ parse = argparse.ArgumentParser(
 )
 
 subparsers = parse.add_subparsers(dest="command")
+
+add_user = subparsers.add_parser("add_user")
+
+add_user.add_argument("--name", required=True)
+add_user.add_argument("--email", required=True)
