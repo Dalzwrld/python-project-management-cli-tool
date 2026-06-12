4eef293b71f306a56654df9f981cfe9257ad814d
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-06-12 10:32:12 +0300

    feat: add task

--- models/task.py
+++ models/task.py
@@ -3,3 +3,13 @@ class Task:
         self.title = title
         self.assigned_to = assigned_to
         self.status = "Pending"
+
+    def mark_complete(self):
+        self.status = "Complete"
+
+    def to_dict(self):
+        return {
+            "title": self.title,
+            "assigned_to": self.assigned_to,
+            "status": self.status
+        }
